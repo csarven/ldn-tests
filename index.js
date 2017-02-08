9d18d4bedf0e792ed9cac7b721c3b63d7d7786e0
Sarven Capadisli <info@csarven.ca>
2017-02-08 17:32:37 +0100

    Add /consumer as an omit route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var atob = require("atob");
 var mayktso = require('mayktso');
 
 var config = mayktso.config();
-mayktso.init({'config': config, 'omitRoutes': ['/media', '/discover-inbox-rdf-body', '/discover-inbox-link-header', '/inbox-compacted/$', '/inbox-expanded/$', '/receiver', '/send-report', '/summary']});
+mayktso.init({'config': config, 'omitRoutes': ['/media', '/consumer', '/discover-inbox-rdf-body', '/discover-inbox-link-header', '/inbox-compacted/$', '/inbox-expanded/$', '/receiver', '/send-report', '/summary']});
 
 mayktso.app.use('/media', mayktso.express.static(__dirname + '/media'));
 mayktso.app.route('/receiver').all(testReceiver);
